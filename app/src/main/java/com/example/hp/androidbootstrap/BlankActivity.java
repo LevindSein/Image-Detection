3264603b23994058668c1ff5a45d72dd5d54dca0
Levind Sein <levindsein@gmail.com>
2019-12-15 19:16:31 +0700

    eleventh commit

--- app/src/main/java/com/example/hp/androidbootstrap/BlankActivity.java
+++ app/src/main/java/com/example/hp/androidbootstrap/BlankActivity.java
@@ -4,23 +4,17 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import java.util.Objects;
 
 public class BlankActivity extends AppCompatActivity {
 
-    static int REQUEST_CODE=7;
-    private ImageView imageView;
-    private TextView textView;
-    private String path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gallery_loader);
-        imageView=(ImageView)findViewById(R.id.imageView);
-        textView=(TextView)findViewById(R.id.textView);
+        setContentView(R.layout.activity_blank);
 
+        Objects.requireNonNull(getSupportActionBar()).hide();
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -29,8 +23,4 @@ public class BlankActivity extends AppCompatActivity {
             }
         }, 500);
     }
-
-//    @Override
-//    public void onBackPressed() {
-//    }
 }
